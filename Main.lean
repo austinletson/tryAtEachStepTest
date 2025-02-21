3252a6bad1d61b917273fdeea37e7c2e2dad633f
austinletson <waustinletson@gmail.com>
2025-02-21 10:59:58 -0500

    init commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import TryAtEachStepTest
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ TryAtEachStepTest.lean
@@ -0,0 +1,9 @@
+-- This module serves as the root of the `TryAtEachStepTest` library.
+-- Import modules here that should be built as part of the library.
+import TryAtEachStepTest.Basic
+
+
+
+theorem test : 1 + 1 = 2 := by 
+  have : 1 = 1 := by rfl
+  rfl
